927861a88f4931c8b0d6a18e5a4741d4f85575d8
Ghizmilth <hidanerferrer@gmail.com>
2017-08-03 16:46:00 -0700

    finished sprint 4

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import TodoModel from '../models/Todo'
 import TodoList from '../components/TodoList'
-
+import CreateTodoForm from '../components/CreateTodoForm'
 
 class TodosContainer extends Component {
   constructor(){
@@ -13,6 +13,7 @@ class TodosContainer extends Component {
   componentDidMount(){
     this.fetchData()
   }
+
   fetchData(){
     TodoModel.all().then( (res) => {
       this.setState ({
@@ -20,9 +21,24 @@ class TodosContainer extends Component {
       })
     })
   }
+
+  createTodo(newBody) {
+    let newTodo = {
+      body: newBody,
+      completed: false
+    }
+    TodoModel.create(newTodo).then((res) => {
+    console.log('created todo', res)
+    let todos = this.state.todos.push(res)
+    this.setState({todos})
+    })
+  }
+
   render(){
     return (
       <div className='todosContainer'>
+        <CreateTodoForm
+          createTodo={this.createTodo.bind(this)} />
         <TodoList
           todos={this.state.todos} />
         </div>
